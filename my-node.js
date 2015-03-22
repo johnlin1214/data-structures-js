1c0e906bb05c9c1f1595c9d3725fc84b093669b3
[01;31m[K"name"[m[K: "John Lin <[01;31m[K"email"[m[K: "johnlin1214@gmail.com>
2015-03-22 09:20:30 -0700

    Added node class

---
+++ my-node.js
@@ -0,0 +1,14 @@
+var myNode = function(value) {
+  this.value = value;
+  this.next = null;
+}
+
+myNode.prototype.addNext = function(nextNode) {
+  this.next = nextNode;
+}
+
+myNode.protoype.removeNext = function() {
+  this.next = null;
+}
+
+module.exports = myNode
